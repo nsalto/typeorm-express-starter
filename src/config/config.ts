1f1b2ffa968c12033f1de8e019216525fdf64fbe
nsalto <nahuelsalto.71@gmail.com>
2022-05-09 21:27:49 -0300

    update

--- src/config/config.ts
+++
@@ -1,3 +0,0 @@
-export const environment = process.env.NODE_ENV;
-export const corsUrl = process.env.CORS_URL;
-export const port = process.env.PORT;
--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import { port } from './config/config';
-import app from './app';
-
-app
-  .listen(port, () => {
-   console.log(`server running on port : ${port}`);
-  })
