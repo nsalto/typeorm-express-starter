cf0ad26c62557c7cdbc1bb713c5c17b1eddb726d
nsalto <nahuelsalto.71@gmail.com>
2022-05-09 21:46:55 -0300

    update

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,11 +1,11 @@
 import { join } from 'path';
 import { DataSource } from 'typeorm';
-import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } from '@config';
+import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } from '../config/index';
 
 const dbConnection = new DataSource({
     type: 'postgres',
     host: DB_HOST,
-    port: DB_PORT,
+    port: 5432,
     username: DB_USER,
     password: DB_PASSWORD,
     database: DB_DATABASE,
