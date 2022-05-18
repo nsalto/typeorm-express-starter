3a73b3db0989ff8ca1c77533155bb9165e6d1809
nsalto <nahuelsalto.71@gmail.com>
2022-05-18 03:53:13 -0300

    handler Err

--- src/middlewares/ErrorHandler.ts
+++ src/middlewares/ErrorHandler.ts
@@ -1,28 +1,29 @@
-import { Request, Response, NextFunction} from 'express'
-import { Error } from 'utils/errors/Error';
+import { Request, Response, NextFunction } from "express";
+import { Error } from "../utils/errors/Error";
 
+export function ErrorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  let errorMessage = {
+    error: {
+      type: "Internal Server Error",
+      message: err.message,
+      code: 500,
+    },
+  };
 
-export function apiErrorHandler(err: Error, req:Request, res:Response, next:NextFunction) {
-    
-    let errorMessage = {
-        error: {
-            type: 'Internal Server Error',
-            message: err.message,
-            code: 500
-        }
-   }
-  
-    if (err instanceof Error) {
+  if (err instanceof Error) {
+    errorMessage.error.type = err.type;
+    errorMessage.error.code = err.code;
+    errorMessage.error.message = err.message;
 
-      errorMessage.error.type = err.type
-      errorMessage.error.code = err.code
-      errorMessage.error.message = err.message
+    res.status(err.code).json(errorMessage);
 
-      res.status(err.code).json(errorMessage);
-
-      return;
-    }
-  
-    res.status(500).json(errorMessage);
-
+    return;
   }
+
+  res.status(500).json(errorMessage);
+}
