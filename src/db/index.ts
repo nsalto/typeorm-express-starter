c818daa314dfe0e5cb388a50c2fd3e7ce8255f9f
nsalto <nahuelsalto.71@gmail.com>
2022-05-18 02:34:16 -0300

    config/db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,13 @@
 import { DataSource } from 'typeorm'
-import { DB_TYPE, DB_HOST, DB_PORT, DB_USERNAME, DB_NAME, DB_PASSWORD } from '../config/index';
+import { DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASSWORD } from '../config/index';
 
-const DB_Connection = new DataSource({
+const dbConnection = new DataSource({
     type: 'postgres',
     host: DB_HOST,
-    port: 5432,
-    username: DB_USERNAME,
+    port: Number(DB_PORT),
+    username: DB_USER,
     password: DB_PASSWORD,
     database: DB_NAME,
 });
 
-export default DB_Connection;
+export default dbConnection;
